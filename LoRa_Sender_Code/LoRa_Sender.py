271d2f2e866d47408a846881c911c76062a4081f
KSLEE7746 <kslee7746@naver.com>
2016-11-24 22:06:35 +0900

    Team LAN : Add serialException handling code
    Signed-off-by: KSLEE7746 <kslee7746@naver.com>

--- LoRa_Sender_Code/LoRa_Sender.py
+++ LoRa_Sender_Code/LoRa_Sender.py
@@ -3,9 +3,24 @@ import serial
 import time
 import linecache
 import sys
-loraSender = serial.Serial('/dev/ttyUSB0', 115200, timeout=3)
-num_lines = 0 #sum(1 for line in open('raw_data.txt'))
+
+num_lines=0
+dev_name='/dev/ttyUSB'
+i=0
 while 1:
+    try:
+        loraSender = serial.Serial(dev_name+str(i), 115200, timeout=3)
+        print("LoRa Connect Success")
+        break
+    except serial.SerialException as e:
+       if (i != 4):
+            i+=1
+            print(e)
+            time.sleep(1)
+       else:
+            i=0
+while 1:
+    print('start flush')
     line = sys.stdin.readline().rstrip('\n')
     loraSender.flushInput()
     loraSender.flushOutput()
